f46703ca5bba912de817fe15acf8294c6d910ea0
abdulrahmanmokhlef <abdulrahman.mokhlef@gmail.com.com>
2021-06-05 19:40:25 +0200

    remove unnecessary code

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -25,34 +25,3 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps)(PrivateRoute)
 
-
-// import React from 'react';
-// import { Route, Redirect } from 'react-router';
-// import { connect } from 'react-redux';
-
-// const PrivateRoute = ({ component: Component, ...rest }) => {
-//   const authed = !!rest.authedUser;
-//   return (
-//     <Route
-//       {...rest}
-//       render={props =>
-//         authed ? (
-//           <Component {...props} />
-//         ) : (
-//           <Redirect
-//             to={{
-//               pathname: '/login',
-//               state: { from: props.location.pathname },
-//             }}
-//           />
-//         )
-//       }
-//     />
-//   );
-// };
-
-// const mapStateToProps = ({ authedUser }) => ({
-//   authedUser,
-// });
-
-// export default connect(mapStateToProps)(PrivateRoute);
